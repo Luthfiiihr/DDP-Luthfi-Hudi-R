3c0fe0719934c7fb340f4cbcf4c399490c4e950e
Luthfiiihr <146084178+Luthfiiihr@users.noreply.github.com>
2023-11-01 10:53:17 +0700

    Add files via upload

---
+++ PEKAN6/cattetan.py
@@ -0,0 +1,23 @@
+#a = 99
+#while a>=1:
+#    print (a)
+#    a-=2
+
+#for i in ["satu,dua,empat"]:
+#    print (i)
+
+#for i in range(0,31,3):
+#    print (i)
+
+#for i in range(50,0,-5):
+#    print(i)
+
+#for i in range(3):
+#    print ("ti03")
+
+#for i in range(1,11):
+#    for j in range(1,11):
+#        k = i*j
+#        print(k,end=" ")
+#    print()
+
---
+++ PEKAN6/tugas2.py
@@ -0,0 +1,7 @@
+
+
+i = 0
+for jumlah in range (1,20,2):
+    i+=jumlah
+
+print("1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 =", i)
