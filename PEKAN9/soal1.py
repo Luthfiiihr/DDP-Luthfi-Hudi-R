f7656af7dd12be7c976b06550643a106d2fdee9b
Luthfiiihr <146084178+Luthfiiihr@users.noreply.github.com>
2023-11-22 20:15:19 +0700

    Add files via upload

---
+++ PEKAN9/soal1.py
@@ -0,0 +1,10 @@
+#Nomer 1
+def profil(nama,alamat,gender,umur,hoby):
+    print("Nama Saya",nama)
+    print("Alamat Saya",alamat)
+    print("Gender Saya",gender)
+    print("Umur Saya",umur)
+    print("Hobby Saya",hoby)
+
+profil("Luthfi","Klapanunggal","Laki-Laki","19 Tahun","Futsal")
+
