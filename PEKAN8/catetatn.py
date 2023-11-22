9def1c099c91f7fa2f0920f5ae41f58953d68a42
Luthfiiihr <146084178+Luthfiiihr@users.noreply.github.com>
2023-11-22 10:23:23 +0700

    Add files via upload

---
+++ PEKAN8/catetatn.py
@@ -0,0 +1,24 @@
+# def jumlah(a,b):
+#     jumlah = a+b
+#     print (jumlah)
+
+# jumlah (3,9)
+
+# def nama_saya(nama, Prodi="BD"):
+#     print("Hallo nama saya adlah",nama)
+#     print("Prodi Saya adlah",Prodi)
+
+
+# nama_saya("ucup","Teknik")
+# nama_saya("yanto", "Sistem")
+# nama_saya("EKO")
+
+# def luas(sisi):
+#     return sisi*sisi
+
+# print (luas(8))
+
+# def pangkat(a,b):
+#     return a**b
+
+# print(pangkat(2,3))
